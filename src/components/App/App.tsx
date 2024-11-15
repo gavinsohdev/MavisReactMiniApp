4ceefe218a357a62e9aa802af99324a02dc754db
vandergav <gavinsohdev@gmail.com>
2024-11-16 01:23:04 +0800

    Add gh-pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import clsx from "clsx";
-import { useShowPopup } from "@vkruglikov/react-telegram-web-app";
+import { useInitData, useWebApp, useShowPopup } from "@vkruglikov/react-telegram-web-app";
 
 export type AppProps = {
   className?: string;
 };
+
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: any;
+    };
+  }
+}
+
 export const App: React.FC<AppProps> = ({ className }) => {
   const showPopup = useShowPopup();
+  const WebApp = useWebApp();
+  const [initData, setInitData] = useState<string>("");
+  const [initDataUnsafe, initData2] = useInitData();
+
+  useEffect(() => {
+    let tg = window.Telegram.WebApp;
+    tg.ready();
+    // setInitData(tg.initDataUnsafe?.user?.first_name);
+    setInitData(JSON.stringify(tg));
+
+    tg.onEvent("mainButtonClicked", function() {
+      const dataToSend = { status: "clicked", timestamp: Date.now() };
+      tg.sendData(JSON.stringify(dataToSend));
+      tg.close();
+    });
+  }, []);
 
   const showPopupOnClick = async () => {
     const message =
       "Thanks for using react-mini-app! I hope it helps you to create awesome Telegram Mini apps!";
     await showPopup({ title: "Hey!", message: message });
+  };
+
+  const mainButtonClickHandler = () => {
+    let tg = window.Telegram.WebApp;
+    tg.MainButton.show();
+    tg.MainButton.setText("Click Me!");
   };
 
   return (
@@ -29,6 +60,15 @@ export const App: React.FC<AppProps> = ({ className }) => {
         <button onClick={showPopupOnClick} className={styles.clickMe}>
           Show popup!
         </button>
+
+        <button onClick={mainButtonClickHandler} className={styles.clickMe}>
+          Show Telegram Main Button
+        </button>
+
+        <div className={styles.initData}>
+          <h2>Init Data:</h2>
+          <p style={{maxWidth: '50vh'}}>{initData}</p> {/* Display the initData */}
+        </div>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { App } from "./components/App/App.tsx";
+import { WebAppProvider } from "@vkruglikov/react-telegram-web-app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <WebAppProvider
+      options={{
+        smoothButtonsTransition: true,
+      }}
+    >
+      <App />
+    </WebAppProvider>
+  </React.StrictMode>
 );
